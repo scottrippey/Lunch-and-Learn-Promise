e2186d5992ff45de9f69224da716ffa5832a24af
Scott Rippey <Scott.William.Rippey@gmail.com>
2014-04-18 10:11:19 -0700

    [Emptied]

---
+++ src/Promise-deferred.js
@@ -0,0 +1,4 @@
+	/** A deferred controls a Promise by exposing resolve, reject, and promise as properties */
+	Promise.deferred = function() {
+
+	};
--- src/Promise.deferred.js
+++
@@ -1,9 +0,0 @@
-	/** A deferred controls a Promise by exposing resolve, reject, and promise as properties */
-	Promise.deferred = function() {
-		var deferred = {};
-		deferred.promise = new Promise(function(resolve, reject) {
-			deferred.resolve = resolve;
-			deferred.reject = reject;
-		});
-		return deferred;
-	};
